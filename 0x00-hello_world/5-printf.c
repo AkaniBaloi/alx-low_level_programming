ccdaa762e333f1d5f708c8c440dab1445b9de4ee
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-05 00:32:41 -0700

    3 task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Prints the numbers since 0 to 9
+ * Description: main - Prints out a string to stdout.
  *
- * Return: Always (Success)
+ * Return: 0 if success.
  */
 int main(void)
 {
-char n;
-for (n= '0'; n <= '9'; n++)
-{
-putchar(n);
-}
-putchar('\n');
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
