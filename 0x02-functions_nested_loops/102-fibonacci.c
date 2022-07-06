ff98b89e550def8c8873c0345de9f097666d23f2
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-06 01:36:08 -0700

    final commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry point
+ * main - block to execute
+ *Desciption: computes and prints
+ *Return: 0
  *
- * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i = 0;
+	long int a = 0, b = 1, next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+
 	return (0);
 }
