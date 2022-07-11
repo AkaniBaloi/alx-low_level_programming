8457d8c7afe17686575df24aa60455a791907142
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-11 03:12:14 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,12 @@
-/*
- * File: 0-strcat.c
- *  Auth: Brennan D Baraban
- */
-#include "main.h"
-
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *  null byte, to the end of the string pointed to by @dest.
- *  @dest: A pointer to the string to be concatenated upon.
+ * reset_to_98 - takes a pointer to an int parameter
+ * and updates the value it points to 98
  *
- *  @src: The source string to be appended to @dest.
+ * @n: int parameter
  *
- *  Return: A pointer to the destination string @dest.
+ * Return: Nothing
  */
-char *strcat(char *dest, const char *src)
+void reset_to_98(int *n)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-	return (dest);
+	*n = 98;
 }
