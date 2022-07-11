07a7aec9aad74da86a9833e324ce0370294fb857
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-11 10:12:30 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
+
 /**
- * _strlen - this function change the initial.
- * @s: this is a parameter to change.
- * Return: len
+ * _strlen - returns the length of a string
+ * @s: string to find length of
+ *
+ * Return: length of s
  */
 int _strlen(char *s)
 {
-	int len;
+	int i = 0;
 
-	while (*s != '\0')
-	{
-		len = len + 1;
-		s = s + 1;
-	}
-	return (len);
+	while (*(s + i))
+		i++;
+	return (i);
+
 }
