ea7ce9c1b065e5a7f6e371e5f9b807e909fac2b6
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-18 03:22:17 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  *set_string - sets the value of a pointer to a char.
  *@s: string;
  *@to: string;
+ *
+ * Return: chessboard.
  */
 
 void set_string(char **s, char *to)
