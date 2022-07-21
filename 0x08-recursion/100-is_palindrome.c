47b6c07d677266252b2163ab6e6f1fdb77ed2f0a
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-21 06:40:56 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,11 +13,11 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * comparator - compares each character of the string
+ * comparator - compares each character of the string.
  * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator.
- * return
+ * Return:
  */
 int comparator(char *s, int n1, int n2)
 {
@@ -31,11 +31,11 @@ int comparator(char *s, int n1, int n2)
 }
 
 /**
- * is_palindrome - checks if a string is a palidrome.
+ * is_palindrome - checks if a string is a palindrome.
  * @s: string.
  * Return: 1 if s is a palindrome, 0 if otherwise.
  */
-int is_palidrome(char *s)
+int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
