19760aae13c897e983e2d3c8a7a4820f2dc4777f
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-23 06:54:21 -0700

    fuctions

--- 0x09-static_libraries/functions/9-strcpy.c
+++ 0x09-static_libraries/functions/9-strcpy.c
@@ -1,23 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * *_strcpy - copies the string pointed to by src, including \0.
- *@src: pointer.
- *@dest: pointer.
- *Return: the pointer to dest.
+ * *_strcpy - copies string pointed by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: copy sting pointed by src
+ * buffer pointed to dest
+ * Return: Pointer to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
-	int i, length;
+	int i = -1;
 
-	for (length = 0; src[length] != '\0'; length++)
-	{
-	}
-
-	for (i = 0; i <= length ; i++)
-	{
+	do {
+		i++;
 		dest[i] = src[i];
-	}
+	} while (src[i] != '\0');
+
 	return (dest);
 }
