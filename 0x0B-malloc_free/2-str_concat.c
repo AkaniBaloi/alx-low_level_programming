18ff98a07fd9b3c4864b2429d0ad02cdcc814b34
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-07-25 02:14:03 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,37 +1,41 @@
-# include "holberton.h"
-# include <stdlib.h>
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *str_concat - concat 2 strings.
- *@s1: first string.
- *@s2: second string.
- *Return: pointer to string.
+ * str_concat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ * Return: pointer should point to a newly allocated space in memory or NULL
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	int i = 0, j = 0, k = 0;
+	char *strnew = NULL;
+	unsigned int i;
+	int n1;
+	int n2;
+	int count;
 
-	if (s1 != NULL)
-		for (; s1[i]; i++)
+	count = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (n1 = 0; s1[n1] != '\0'; n1++)
 		;
-
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
 		return (NULL);
-
-	while (k < i)
-	{
-		s[k] = s1[k];
-		k++;
 	}
-
-	while (k < i + j)
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (; s2[count] != '\0'; i++)
 	{
-		s[k] = s2[k - i];
-		k++;
+		strnew[i] = s2[count];
+		count++;
 	}
-	s[k] = '\0';
-	return (s);
+	return (strnew);
 }
