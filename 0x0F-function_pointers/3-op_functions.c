6d0964964c0b6f64c468f57d057f5a808e485ff6
AkaniBaloi <AkaniBaloi08@gmail.com>
2022-08-03 05:57:04 -0700

    all task

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,29 +1,68 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
-/**
- * get_op_func - Find the equivalent function.
- * @s: Operand.
- * Return: Function address.
- **/
-int (*get_op_func(char *s))(int, int)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+#include <stdio.h>
 
-	i = 0;
-	while (ops[i].op != NULL)
+/**
+ * op_add - Add two numbers.
+ * @a: First number.
+ * @b: Second number.
+ * Return: integer.
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substitute a number from another one.
+ * @a: First number.
+ * @b: Second number.
+ * Return: integer.
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiply a number by another one.
+ * @a: First number.
+ * @b: Second number
+ * Return: integer.
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide a number by another one.
+ * @a: First number.
+ * @b: Second number.
+ * Return: integer.
+ **/
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			break;
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (ops[i].f);
+	return (a / b);
+}
+
+/**
+ * op_mod - Get the rest of division of a number by another one.
+ * @a: First number.
+ * @b: Second number.
+ * Return: integer.
+ **/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
